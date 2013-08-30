bfdac2e262a38554915eca1c25ae1530c9c377c4
Michael Chrisco <michaelachrisco@gmail.com>
2013-08-29 20:17:50 -0700

    First bits

--- development/music_db/song.rb
+++ development/music_db/song.rb
@@ -1,10 +1,3 @@
 class Song
-  @artist = 'The Progues'
-
-  def artist; 'Led Zeppelin'; end
-  def get_the_artist; @artist; end
-
-  def class_artist
-    self.class.instance_variable_get(:@artist)
-  end
+  attr_accessor :title, :artist, :album, :track, :notes
 end
