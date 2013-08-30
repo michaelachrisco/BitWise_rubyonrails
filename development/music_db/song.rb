3136b8409f3a605e33fe5ce425b7831257ece230
Michael Chrisco <michaelachrisco@gmail.com>
2013-08-29 21:04:44 -0700

    stopping place for tonight, dump def

--- development/music_db/song.rb
+++ development/music_db/song.rb
@@ -1,3 +1,34 @@
+require "yaml"
+
 class Song
   attr_accessor :title, :artist, :album, :track, :notes
+
+  def to_s
+    output = "Title: #{@title}\n"
+    output << "Artist: #{@artist}\n"
+    output << "Album: #{@album}\n"
+    output << "Track: #{@track}\n"
+
+    output
+  end
+  
+  def to_yaml
+    the_song = {
+      title: @title,
+      artist: @artist,
+      album: @album,
+      track: @track
+    }
+
+    YAML.dump(the_song)
+  end
+
+  def dump(filename=nil)
+    filename ||= @title
+    filename ||= 'unknown'
+
+    File.write("#{filename}.yml", to_yaml)
+  end
+
+
 end
