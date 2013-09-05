e03406898496808dbd084cccb78b60e15b434bee
Michael Chrisco <michaelachrisco@gmail.com>
2013-09-04 20:15:02 -0700

    refactor

--- development/music_db/lib/music_db/song.rb
+++ development/music_db/lib/music_db/song.rb
@@ -1,8 +1,19 @@
-require 'yaml'
+require_relative 'yamlable'
 
 
-class Song
+module MusicDB
+
+ class Song
+  include YAMLable
+
   attr_accessor :title, :artist, :album, :track
+
+  def initialize(title, artist, album=nil, track=nil)
+    @title = title
+    @artist = artist
+    @album = album
+    @track = track
+  end
 
   def to_s
     output = "title: #{@title}\n"
@@ -13,21 +24,13 @@ class Song
     output
   end
 
-  def to_yaml
-    the_song = {
+  def attributes
+    {
       title: @title,
       artist: @artist,
       album: @album,
       track: @track
     }
-
-    YAML.dump(the_song)
   end
-
-  def dump(filename=nil)
-    filename ||= @title
-    filename ||= 'unknown'
-
-    File.write("#{filename}.yml", to_yaml)
-  end
+ end
 end
