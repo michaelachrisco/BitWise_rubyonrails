7e7995d5da3cb25a9628b8c313c089f5e78f81b3
Michael Chrisco <michaelachrisco@gmail.com>
2013-09-12 18:29:21 -0700

    Added first validation

--- development/flocker/db/schema.rb
+++ development/flocker/db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130912034816) do
+ActiveRecord::Schema.define(version: 20130913011340) do
 
   create_table "photos", force: true do |t|
     t.string   "filename"
     t.string   "path"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
--- development/flocker/spec/models/user_spec.rb
+++ development/flocker/spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
+
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'without an email address' do
+    it 'is not valid' do
+      user = User.new
+      user.should_not be_valid
+    end
+
+    it 'fails validation' do
+      expect(User.new).to have(1).error_on(:email)
+    end
+  end
 end
