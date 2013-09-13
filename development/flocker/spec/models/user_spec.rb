34d523c5c53c410d18a888fd6aacbacaea3dee12
Michael Chrisco <michaelachrisco@gmail.com>
2013-09-12 19:10:04 -0700

    Added validation on email uniqueness

--- development/flocker/spec/models/user_spec.rb
+++ development/flocker/spec/models/user_spec.rb
@@ -12,4 +12,18 @@ describe User do
       expect(User.new).to have(1).error_on(:email)
     end
   end
+  context 'not unique email address' do
+    it 'fails validation' do
+     User.create!(username: 'test', email: 'test@test.com')
+       expect(User.new(email: 'test@test.com')).to have(1).error_on(:email)
+     end 
+  end 
+    context 'not unique username' do
+    it 'fails validation' do
+     User.create!(username: 'test', email: 'test@test.com')
+       expect(User.new(username: 'test')).to have(1).error_on(:username)
+     end
+  end
+
+
 end
