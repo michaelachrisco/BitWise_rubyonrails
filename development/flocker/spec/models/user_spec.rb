1006f840596a73c9636cdafbf3f2d723836b3a7d
Michael Chrisco <michaelachrisco@gmail.com>
2013-09-12 19:36:47 -0700

    Associated user and photo

--- development/flocker/spec/models/user_spec.rb
+++ development/flocker/spec/models/user_spec.rb
@@ -3,27 +3,21 @@ require 'spec_helper'
 
 describe User do
   context 'without an email address' do
-    it 'is not valid' do
-      user = User.new
-      user.should_not be_valid
-    end
-
     it 'fails validation' do
       expect(User.new).to have(1).error_on(:email)
     end
   end
-  context 'not unique email address' do
+
+  context 'with a duplicate email address' do
     it 'fails validation' do
-     User.create!(username: 'test', email: 'test@test.com')
-       expect(User.new(email: 'test@test.com')).to have(1).error_on(:email)
-     end 
-  end 
-    context 'not unique username' do
-    it 'fails validation' do
-     User.create!(username: 'test', email: 'test@test.com')
-       expect(User.new(username: 'test')).to have(1).error_on(:username)
-     end
+      User.create!(email: 'test@test.com')
+      expect(User.new(email: 'test@test.com')).to have(1).error_on(:email)
+    end
   end
 
-
+  it 'can have many photos' do
+    user = User.new(email: 'test@test.com')
+    2.times { user.photos << Photo.new }
+    expect(user).to be_valid
+  end
 end
